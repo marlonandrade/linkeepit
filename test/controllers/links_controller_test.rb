e42d79fee91a46d83c3a42e750075580043bb8ee
Marlon Andrade <marlonmandrade@gmail.com>
2014-11-18 19:01:01 -0200

    Adding some model validation
    - adjusting controller tests for signed_out path

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -2,45 +2,55 @@ require 'test_helper'
 
 class LinksControllerTest < ActionController::TestCase
   setup do
-    @link = links(:one)
+    @link = links(:google)
     sign_in :user, users(:homer)
   end
 
-  test "should get index" do
+  test 'should redirect when signed out' do
+    sign_out :user
+    get :index
+    assert_response :found
+  end
+
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:links)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create link" do
+  test 'should create link' do
     assert_difference('Link.count') do
-      post :create, link: { url: @link.url }
+      post :create, link: { 
+        url: 'http://g1.globo.com' 
+      }
     end
 
     assert_redirected_to link_path(assigns(:link))
   end
 
-  test "should show link" do
+  test 'should show link' do
     get :show, id: @link
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @link
     assert_response :success
   end
 
-  test "should update link" do
-    patch :update, id: @link, link: { url: @link.url }
+  test 'should update link' do
+    patch :update, id: @link, link: {
+      url: @link.url 
+    }
     assert_redirected_to link_path(assigns(:link))
   end
 
-  test "should destroy link" do
+  test 'should destroy link' do
     assert_difference('Link.count', -1) do
       delete :destroy, id: @link
     end
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save link without url' do
+    assert_not Link.new.save, 'saved link without url'
+  end
+
+  test 'should not save link with duplicate url' do
+    link = Link.new url: links(:google).url
+    assert_not link.save, 'saved link with duplicated url'
+  end
 end
