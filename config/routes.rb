99dbe6fafaa1fd7bd4999a06bfd022b7b1c62a08
Marlon Andrade <marlonmandrade@gmail.com>
2014-11-17 22:01:37 -0200

    Adding devise and scaffold for links
    - initial setup to see if everything is working fine on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :links
+
+  devise_for :users
+
+  root 'links#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
