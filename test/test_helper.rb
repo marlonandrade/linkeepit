3c2a43a6a19cdc7dfd5bd8345ea456933052288a
Marlon Andrade <marlonmandrade@gmail.com>
2014-11-18 19:23:16 -0200

    creating a helper for signing in
    - including it on integration tests
    - adding capybara setup on test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'test_helpers/session_helper'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -11,4 +13,9 @@ end
 
 class ActionController::TestCase
   include Devise::TestHelpers
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include TestHelpers::SessionHelper
 end
